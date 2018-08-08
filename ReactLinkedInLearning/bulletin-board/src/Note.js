1f4f9bdcf2967ca006991c98036be4fd34e6bf97
Harish sv <svharish2000@live.com>
2018-08-08 21:43:18 +0400

    Commited board fn

--- ReactLinkedInLearning/bulletin-board/src/Note.js
+++ ReactLinkedInLearning/bulletin-board/src/Note.js
@@ -1,32 +1,70 @@
 import React, { Component } from 'react'
 import { FaTrash } from 'react-icons/fa';
 import { FaPencilAlt } from 'react-icons/fa';
+import { FaSave } from 'react-icons/fa';
 
 class Note extends Component {
 	constructor(props) {
 		super(props)
-		this.edit = this.edit.bind()
-		this.remove = this.remove.bind()
+
+		this.state = {
+			editing: false
+		}
+
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+		this.renderForm = this.renderForm.bind(this)
 	}
 
 	edit() {
-		alert('editing note')
+		this.setState({
+			editing: true
+		})
 	}
 
 	remove() {
-		alert('removing note')
+		this.props.onRemove(this.props.index)
 	}
 
-	render() {
+	save(e) {
+		e.preventDefault()
+		this.props.onChange(this._newText.value, this.props.index)
+		this.setState({
+			editing: false
+		})
+	}
+
+	renderForm() {
 		return (
 			<div className="note">
-				<p>Learn React</p>
+				<form onSubmit= {this.save }>
+					<textarea ref={input => this._newText = input}/>
+					<button id="save"><FaSave/></button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} id="edit"><FaPencilAlt /></button>
 					<button onClick={this.remove} id="remove"><FaTrash /></button>
 				</span>
 			</div>
 		)
+	}
+
+	render() {
+		if (this.state.editing) {
+			return this.renderForm()
+		} else {
+			return this.renderDisplay()
+		}
 	}
 }
 
